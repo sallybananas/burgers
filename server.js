5c69a33f02d7f5235e3a82e5a6d66251116d3585
sallybananas <saljgiani@gmail.com>
2018-01-13 18:40:18 -0700

    fixes

--- server.js
+++ server.js
@@ -8,11 +8,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
-
+var path = require('path')
 var app = express();
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('./public'));
-// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
+
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
